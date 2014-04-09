e3aaa7c653d70b5afc7a28ac0cfe38df3b636405
jay16 <jay_li@intfocus.com>
2014-04-09 22:33:06 +0800

    fuck git merge

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -5,9 +5,4 @@ module RecordsHelper
     (record.tags.map { |t| t.label } + record.tags_list.split(","))
     .uniq.map { |l| content_tag(:li, l) }.join.html_safe
   end
-
-  def tag_it_list tags
-    return if !tags.size
-    tags.map { |t| content_tag(:li, t.label) }.join.html_safe
-  end
 end
