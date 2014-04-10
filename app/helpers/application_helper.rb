ac555aedf00b4d32c7993f00649c333e69329cad
jay16 <jay_li@intfocus.com>
2014-04-10 22:52:34 +0800

    devise#sign_out && notice_message with notice and alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,16 @@ module ApplicationHelper
     end
   end
 
+  def notice_message
+    flash_messages = []
+    flash.each do |type, message|
+      type = :success if type == :notice
+      text = content_tag(:div, link_to("x", "#", class: "close", "data-dismiss" => "alert") + message, class: "alert alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
+
   def controller_javascript_include_tag
   end
 
