e9248f1699a9e4d88dcb42d857aa1f15da916ad9
jay16 <jay_li@intfocus.com>
2014-04-13 16:46:16 +0800

    标签修改

--- app/models/record.rb
+++ app/models/record.rb
@@ -35,18 +35,14 @@ class Record < ActiveRecord::Base
   end
 
   def tags_string
-    self.tags.map { |t| t.label }.join(",")
-  end
-
-  def tags_string
-    self.tags.map { |t| t.label }.join(",") 
+    self.tags.map(&:label).join(",") 
   end
 
   def build_with_tags
-    return if self.tags_list.blank? or self.tags_list.chomp.size == 0
+    return if self.tags_list.chomp.empty?
      
-    tags = self.tags.map { |t| t.label }
-    self.tags_list.split(",").map{ |t| t.strip }.uniq.each do |label|
+    tags = self.tags.map(&:label)
+    self.tags_list.split(",").map(&:strip).uniq.each do |label|
        tag = self.user.tags.find_or_create_by(label: label)
        tags.include?(label) ? tags.delete(label) : self.tags << tag
     end
