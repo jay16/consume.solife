bcde022250c68d7c2eb15ba2a4972d8735a2f0e6
jay16 <jay_li@intfocus.com>
2014-07-24 17:12:47 +0800

    css#font zapfino

--- passenger.sh
+++
@@ -1,8 +0,0 @@
-echo "Sync Consume DB from remote VPS In Background!"
-nohup /usr/local/var/rbenv/shims/ruby /Users/lijunjie/Code/crond/sync_consume_db.rb > ./log/nohup.syncDB.log 2>&1 &
-
-echo "Make Sure MySQL start!"
-mysql.server start
-
-#sudo lsof -i:3000
-passenger start -e production
