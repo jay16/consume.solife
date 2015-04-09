11238da736cfb144d989481e8083191ebb092105
jay16 <jay_li@intfocus.com>
2015-04-09 16:16:16 +0800

    annotation lib/utils/bkmg.rb

--- lib/utils/bkmg.rb
+++ lib/utils/bkmg.rb
@@ -1,45 +1,80 @@
+#encoding: utf-8
+#   Computer storage measuremen conversion.
+#     1 B = 1 byte = 8 bit 
+#     1 KB = 1024 bytes
+#     1 MB = 1024 KB
+#     1 GB = 1024 MB
+#
+#   author: jay(jay_li@solife.us) 
+#   date: 2015/04/07
+#
+#   usage:
+#
+#   BKMG::Base.to("123 b", "G", -1) => 1.1455267667770386e-07G
+#   BKMG::Base.best("123 b") => 123B
+#   BKMG::Base.to("123 kb", "G", -1) => 0.00011730194091796875G
+#   BKMG::Base.best("123 kb") => 123K
+#   BKMG::Base.to("2423 mb", "G", -1) => 2.3662109375G
+#   BKMG::Base.best("2423 mb") => 2G
+#   BKMG::Base.to("1424 g", "G", -1) => 1424G
+#   BKMG::Base.best("1424 g") => 1424G
+#
 module BKMG
   class Base 
     class << self
       UNIT_SEQ = %w(B K M G)
-      # 1 B = 1 byte = 8 bit 
-      # 1 KB = 1024 bytes
-      # 1 MB = 1024 KB
-      # 1 GB = 1024 MB
-      def deal(str)
-        str  = str.upcase.gsub(/\s+/, "")
-        str.scan(/(\d+)(#{UNIT_SEQ.join("|")})/)[0]
+
+      # turn up to target unit
+      # params:
+      #   str: size string and unit
+      #   to_unit: target unit
+      #   ndigits: precision in decimal digits (default 0 digits)
+      #            don't truncate when ndigits lower than zero
+      def to(str, to_unit, ndigits=0)
+        _to(str, to_unit, ndigits) rescue str
       end
-      def to(str, to_unit, decimal=1)
-        _to(str, to_unit, decimal) rescue str
+
+      # turn up and numberic great zero
+      # params:
+      #   str: size string and unit 
+      #   ndigits: keep dot position
+      #            don't truncate when ndigits lower than zero
+      def best(str, ndigits=1)
+        _best(str, ndigits) rescue str
       end
-      def _to(str, to_unit, decimal=1)
-        to_unit = to_unit.upcase
-        num, unit = BKMG::Base.deal(str)
-        diff = UNIT_SEQ.index(to_unit) - UNIT_SEQ.index(unit)
-        if diff > 0
-          (num.to_f/1024**diff).round(decimal).to_s + to_unit
-        else
-          [num, unit].join 
+
+      # assist methods
+      def _deal(str)
+        str.upcase.gsub(/\s+/, "").scan(/(\d+)(#{UNIT_SEQ.join("|")})/)[0]
+      end
+
+      def _to(str, to_unit, ndigits=1)
+        to_unit.upcase!
+        num, unit = _deal(str)
+        if (diff = UNIT_SEQ.index(to_unit) - UNIT_SEQ.index(unit)) > 0
+          num  = num.to_f/1024**diff
+          num  = num.round(ndigits) if ndigits >= 0
+          unit = to_unit 
         end
+        [num, unit].join 
       end
-      def best(str, decimal=1)
-        _best(str, decimal) rescue str
-      end
-      def _best(str, decimal=1)
-        num, unit = BKMG::Base.deal(str)
+
+      def _best(str, ndigits=1)
+        num, unit = _deal(str)
         index = UNIT_SEQ.index(unit)
-        while index <= UNIT_SEQ.length - 1 and num.to_f/1024 > 1
+        while index < UNIT_SEQ.length - 1 and num.to_f/1024 > 1
           num = num.to_f/1024
           index += 1
         end
-        [num.to_f.round(decimal), UNIT_SEQ[index]].join
+        num = num.to_f.round(ndigits) if ndigits >= 0
+        [num, UNIT_SEQ[index]].join
       end
     end
   end
 end
 
 #["2124", "asdfa k", "123 b", "123 kb", "123 k","2423 mb", "1424 g"].each_with_index do |str, index|
-#  puts "%s - %s" % [str, BKMG::Base.to(str, "g", index+1).to_s]
+#  puts "%s - %s" % [str, BKMG::Base.to(str, "g", -1).to_s]
 #  puts "%s - %s" % [str, BKMG::Base.best(str, 0)]
+#  puts "="*10
 #end
