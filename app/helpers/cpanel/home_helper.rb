11546237f49bbd836886eb4f24ca36088b872ad8
jay16 <jay_li@intfocus.com>
2015-04-15 13:05:58 +0800

    system(bash-script) with rescue.

--- app/helpers/cpanel/home_helper.rb
+++ app/helpers/cpanel/home_helper.rb
@@ -1,9 +1,9 @@
 module Cpanel::HomeHelper
   def app_disk_info
-    one   = `du -sh #{Rails.root}`.split.first
-    two   = `du -sh #{Rails.root}/tmp`.split.first
-    three = `du -sh #{Rails.root}/vendor`.split.first
-    four  = `du -sh #{Rails.root}/log`.split.first
+    one   = `du -sh #{Rails.root}`.split.first rescue "E"
+    two   = `du -sh #{Rails.root}/tmp`.split.first rescue "E"
+    three = `du -sh #{Rails.root}/vendor`.split.first rescue "E"
+    four  = `du -sh #{Rails.root}/log`.split.first rescue "E"
     "#{one}(vendor:#{three}, log:#{four}, tmp: #{two})"
   end
 
